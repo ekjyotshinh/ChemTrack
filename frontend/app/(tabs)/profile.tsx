e6711003c687053a6ecb84668abdce0b32b83e50
Katy Chan <121604663+Kren1375@users.noreply.github.com>
2024-11-03 18:29:24 -0800

    #CT-90 View Profile Page #Added Profile Page Placeholder Displays Profile Page

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,22 +1,279 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Button, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions, Modal, Pressable } from 'react-native';
 
 export default function ViewChemicals() {
+  const [name, setName] = React.useState(users[0].name);
+  const [email, setEmail] = React.useState(users[0].email);
+  const [initials, setInitials] = React.useState('');
+
+  //Function to get initials of user
+  const getInitals = (someName: string) => {
+    const fullName = someName.split(' ');
+    let initials = '';
+    for (let i = 0; i < fullName.length; i++) {
+      initials = initials.concat(fullName[i].charAt(0));
+    }
+    return initials;
+  };
+
+
+  // Function to set the avatar as initials
+  const defaultAvatar = () => {
+    const [initials, setIntials] = React.useState('');
+
+    //return setInitials(getInitals(users[0].name));
+    return getInitals(users[0].name);
+  };
+
+  const buttonPopUp = () => {
+
+  };
+
+
+
   return (
+
     <View style={styles.container}>
-      <Text style={styles.title}>Profile</Text>
+      <Text style={styles.title}>My <Text style={styles.titleSecondWord}>Account</Text></Text>
+
+
+      <View style={styles.avatarContainer}>
+        <View style={styles.avatarImage}>
+          <Text style={styles.avatarText}> {defaultAvatar()}</Text>
+
+        </View>
+      </View>
+
+      <View>
+        <TouchableOpacity>
+
+          <Text style={styles.editText}>Edit</Text>
+
+        </TouchableOpacity>
+      </View>
+
+
+      <ScrollView>
+        <View >
+          <Text style={styles.inputHeader}>Name</Text>
+          <TextInput style={styles.inputBox}
+            onChangeText={name => setName(name)}
+
+            placeholderTextColor='black'
+            placeholder={name}
+            value={name}
+          />
+
+        </View>
+
+        <View>
+          <Text style={styles.inputHeader}>Email</Text>
+          <TextInput style={styles.inputBox}
+            onChangeText={setEmail}
+            placeholderTextColor='black'
+            placeholder={email}
+            value={email}
+          />
+
+        </View>
+
+        <View style={styles.buttonContainer}>
+
+          {/*Invite User button */}
+          <View>
+            <TouchableOpacity style={styles.functionButtons}>
+              <TouchableOpacity><Text style={styles.functionButtonsText}>Invite User</Text></TouchableOpacity>
+
+            </TouchableOpacity>
+          </View>
+
+
+          {/*Notifications button */}
+          <View>
+            <TouchableOpacity style={styles.functionButtons}>
+              <Text style={styles.functionButtonsText}>Notifications</Text>
+
+            </TouchableOpacity>
+          </View>
+
+
+          {/*Reset Password button */}
+          <View>
+            <TouchableOpacity style={styles.functionButtons}>
+              <Text style={styles.functionButtonsText}>Reset Password</Text>
+            </TouchableOpacity>
+          </View>
+
+
+          {/*Log Out button */}
+          <View>
+            <TouchableOpacity style={styles.logOutButton}>
+              <Text style={styles.logOutButtonText}>Log Out</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+
+
     </View>
   );
 }
 
+
+{/*Example User Data */ }
+
+/* TypeScript data testing
+let Users: {name: string, email:string, password:string}[] = [
+  {
+    name: 'Admin John',
+    email: 'admin@example.com',
+    password: 'super123',
+  },
+  {
+    name: 'User Bob',
+    email: 'user@example.com',
+    password: 'user123',
+  },
+];
+*/
+
+let users: { name: string, email: string, password: string }[] = [
+  {
+    name: 'Admin John',
+    email: 'admin@example.com',
+    password: 'super123',
+  },
+  {
+    name: 'User Bob',
+    email: 'user@example.com',
+    password: 'user123',
+  },
+];
+
+{/*StyleSheet */ }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F5F5F5',
+
   },
   title: {
-    fontSize: 20,
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#4285F4',
+    paddingHorizontal: 20,
+    paddingTop: 35,
+    paddingBottom: 15,
+  },
+  titleSecondWord: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'black',
+
+  },
+  avatarContainer: {
+    alignSelf: 'center',
+  },
+  avatarImage: {
+    width: 140,
+    height: 140,
+    borderRadius: 70,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    margin: 5,
+
+    shadowColor: 'grey',
+    shadowRadius: 0.5,
+    shadowOpacity: 2,
+    shadowOffset:
+    {
+      height: 1,
+      width: 0.2,
+    },
+
+  },
+  avatarText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  editText: {
+    color: '#4285F4',
+    alignSelf: 'center',
+    fontWeight: 600,
+    fontSize: 15,
+  },
+  inputHeader: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    margin: 5,
+    paddingHorizontal: 20,
+  },
+  inputBox: {
+    fontSize: 15,
+    borderColor: 'silver',
+    borderWidth: 1,
+    color: 'black',
+    flex: 1,
+    backgroundColor: 'white',
+    width: Dimensions.get('screen').width - 50,
+    paddingHorizontal: 7,
+    paddingVertical: 8,
+    margin: 6,
+    alignSelf: 'center',
+  },
+  buttonContainer: {
+    padding: 22,
+  },
+
+  functionButtons: {
+    backgroundColor: 'white',
+    paddingHorizontal: 50,
+    paddingVertical: 20,
+    borderRadius: 8,
+    margin: 10,
+    width: Dimensions.get('screen').width - 55,
+    alignSelf: 'center',
+
+    shadowColor: 'grey',
+    shadowRadius: 0.5,
+    shadowOpacity: 1,
+    shadowOffset:
+    {
+      height: 1,
+      width: 0.1,
+    },
+  },
+  functionButtonsText: {
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+
+  },
+  logOutButton: {
+    backgroundColor: 'red',
+    paddingHorizontal: 100,
+    paddingVertical: 20,
+    borderRadius: 8,
+    margin: 10,
+    width: Dimensions.get('screen').width - 55,
+    alignSelf: 'center',
+
+    shadowColor: 'silver',
+    shadowRadius: .8,
+    shadowOpacity: 1,
+    shadowOffset:
+    {
+      height: 1,
+      width: 1,
+    },
+  },
+  logOutButtonText: {
+    color: 'white',
+    textAlign: 'center',
     fontWeight: 'bold',
   },
 });
