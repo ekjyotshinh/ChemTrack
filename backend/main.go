064487bf3070a4622aa46b4802d9b953d4f93af9
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2024-11-11 12:20:45 -0800

    set up .env file for backend & cors error fix

--- backend/main.go
+++ backend/main.go
@@ -2,20 +2,39 @@ package main
 
 import (
     "log"
+	"os"
 
     "github.com/swaggo/gin-swagger"
     "github.com/swaggo/files"
     "github.com/gin-gonic/gin"
     "github.com/ekjyotshinh/ChemTrack/backend/routes"
     _ "github.com/ekjyotshinh/ChemTrack/backend/docs" // Import generated docs
+	"github.com/gin-contrib/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	// Load environment variables from .env
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Create a Gin router
+    router := gin.Default()
+
+	// Set up CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{os.Getenv("FRONTEND_PORT")},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+	}))
+
+
     // Initialize Firestore
     routes.InitFirestore()
 
-    // Create a Gin router
-    router := gin.Default()
+
 
     // Register routes
     routes.RegisterRoutesUser(router)
