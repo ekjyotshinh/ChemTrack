8c9d3ec52170b57afc696ce356f2113c427144fa
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-01-28 23:14:04 -0800

    updated to have the landing acreen as start screen of the app

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -33,7 +33,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: '(auth)',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
