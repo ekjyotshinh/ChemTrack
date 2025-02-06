7ed23e7c0ed3bcdfcd0ebb8195f778fecb2399ac
rahulio96 <rahgupta96@gmail.com>
2025-02-05 20:30:24 -0800

    Make separate component for modal container and add dashes to CAS

--- frontend/components/viewChemicalModals/ChemicalDetails.tsx
+++ frontend/components/viewChemicalModals/ChemicalDetails.tsx
@@ -1,13 +1,44 @@
-import { BlurView } from 'expo-blur'
-import { View, Modal, ScrollView, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Modal, ScrollView, StyleSheet } from 'react-native'
+import { ReactNode } from 'react'
 import Colors from '@/constants/Colors';
-import CloseIcon from '@/assets/icons/CloseIcon';
 import TextInter from '../TextInter';
 import Size from '@/constants/Size';
 import CustomButton from '../CustomButton';
 import QrCodeIcon from '@/assets/icons/QRCodeIcon';
 import ViewDocIcon from '@/assets/icons/ViewDocIcon';
 import CustomEditIcon from '@/assets/icons/EditIcon';
+import ModalContainer from './ModalContainer';
+
+interface chemicalDetailProps {
+    property: string
+    value: string | null
+    color?: string
+    margin?: number
+}
+
+// Component for displaying chemical details (attributes such as school, status, etc.)
+const ChemicalDetail = ({ property, value, color, margin = 0 }: chemicalDetailProps) => {
+    if (!color) color = Colors.black
+    return (
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginBottom: margin }}>
+            <TextInter style={{ fontWeight: '700', fontSize: 18 }}>{property}</TextInter>
+            <TextInter style={{ color: color, fontSize: 18 }}>{value || 'Unknown'}</TextInter>
+        </View>
+    )
+}
+
+// Scroll container with flexbox styling
+const ScrollContainer = ({ children }: { children: ReactNode }) => {
+    return (
+        <View style={stylesPopup.modalContent}>
+            <ScrollView style={{ width: '100%' }}>
+                <View style={{ paddingBottom: 75, paddingHorizontal: 35 }}>
+                    {children}
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
 
 interface Chemical {
     id: string;
@@ -28,20 +59,11 @@ interface props {
     closeModal: () => void
 }
 
-const ChemicalDetail = ({ property, value, color }: { property: string, value: string | null, color?: string }) => {
-    if (!color) color = Colors.black
-    return (
-        <View style={stylesPopup.details}>
-            <TextInter style={{ fontWeight: '700', fontSize: 18 }}>{property}</TextInter>
-            <TextInter style={{ color: color, fontSize: 18 }}>{value || 'Unknown'}</TextInter>
-        </View>
-    )
-}
-
 const ChemicalDetails = ({ selectedChemical, toggleSDSBottomSheet, modalVisible, closeModal }: props) => {
 
-    const gapSize = Size.height(15)
+    const gapSize = Size.height(12)
 
+    // Determine color based on location status
     const getStatusColor = (status: string) => {
         switch (status) {
             case 'On-site':
@@ -53,6 +75,7 @@ const ChemicalDetails = ({ selectedChemical, toggleSDSBottomSheet, modalVisible,
         }
     }
 
+    // Determine color based on quantity level
     const getQuantityColor = (quantity: string) => {
         switch (quantity) {
             case 'Good':
@@ -66,128 +89,111 @@ const ChemicalDetails = ({ selectedChemical, toggleSDSBottomSheet, modalVisible,
         }
     }
 
+    // Adds dashes to the CAS number
+    const processCAS = (CAS: string) => {
+        if (CAS.length === 0) return 'Unknown'
+
+        let result = ''
+        let string = CAS.toString()
+
+        for (let i = 0; i < string.length; i++) {
+            if (i === string.length - 1 || i === string.length - 3) {
+                result += '-'
+            }
+            result += string[i]
+        }
+        return result
+    }
+
     return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={closeModal}>
-            <BlurView intensity={50} style={stylesPopup.blurContainer}>
-                <View style={stylesPopup.modalView}>
+        <ModalContainer modalVisible={modalVisible} closeModal={closeModal}>
+            <ScrollContainer>
+                {selectedChemical && (<>
+                    <View style={stylesPopup.qrTitleContainer}>
+                        {/* QR Code Placeholder */}
+                        <View style={stylesPopup.qrCodePlaceholder}>
+                            <TextInter>QR Code</TextInter>
+                        </View>
 
-                    {/* X (Close) button */}
-                    <TouchableOpacity style={stylesPopup.closeButton} onPress={closeModal}>
-                        <CloseIcon width={24} height={24} color={Colors.black} />
-                    </TouchableOpacity>
-
-                    <ScrollView contentContainerStyle={stylesPopup.modalContent}>
-                        {/* Chemical Details */}
-                        {selectedChemical && (<>
-                            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'center', marginBottom: Size.height(10) }}>
-                                {/* QR Code Placeholder */}
-                                <View style={stylesPopup.qrCodePlaceholder}>
-                                    <TextInter>QR Code</TextInter>
-                                </View>
-                            
-                                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', marginLeft: 5 }}>
-                                    <TextInter style={stylesPopup.chemicalName}>{selectedChemical.name}</TextInter>
-                                    <ChemicalDetail property={'CAS: '} value={selectedChemical.CAS} />
-                                </View>
-                            </View>
+                        {/* Chemical Name & CAS */}
+                        <View style={stylesPopup.nameCASContainer}>
+                            <TextInter style={stylesPopup.chemicalName}>{selectedChemical.name}</TextInter>
+                            <ChemicalDetail property={'CAS: '} value={processCAS(selectedChemical.CAS)} />
+                        </View>
+                    </View>
 
 
-                            {/* Chemical Details */}
-                            <ChemicalDetail property={'ID: '} value={selectedChemical.id} />
-                            <View style={{flex: 1, marginBottom: gapSize, marginTop: gapSize}}>
-                                <ChemicalDetail property={'Purchase Date: '} value={selectedChemical.purchase_date} />
-                                <ChemicalDetail property={'Expiration Date: '} value={selectedChemical.expiration_date} />
-                            </View>
-                            <ChemicalDetail property={'School: '} value={selectedChemical.school} />
-                            <ChemicalDetail property={'Room: '} value={selectedChemical.room} />
-                            <ChemicalDetail property={'Cabinet: '} value={selectedChemical.cabinet} />
-                            <ChemicalDetail property={'Shelf: '} value={selectedChemical.shelf} />
+                    {/* Chemical Details */}
+                    <ChemicalDetail property={'ID: '} value={selectedChemical.id} margin={gapSize} />
 
-                            {/* Status */}
-                            <View style={{flex: 1, marginBottom: gapSize, marginTop: gapSize}}>
-                                <ChemicalDetail property={'Status: '} value={'On-site'} color={getStatusColor('On-site')} />
-                                <ChemicalDetail property={'Quantity: '} value={'Good'} color={getQuantityColor('Good')} />
-                            </View>
-                            
-                            <View style={stylesPopup.buttonContainer}>
-                                <CustomButton 
-                                    title={'Save QR Label'}
-                                    icon={<QrCodeIcon color={Colors.black} strokeWidth='4' />}
-                                    textColor={Colors.black}
-                                    onPress={()=> {}}
-                                    color={Colors.white}
-                                    width={270}
-                                    height={47}
-                                    fontSize={14}
-                                />
-                                <CustomButton 
-                                    title={'View SDS'}
-                                    icon={<ViewDocIcon color={Colors.black} />}
-                                    textColor={Colors.black}
-                                    onPress={()=> {}}
-                                    color={Colors.white}
-                                    width={270}
-                                    height={47}
-                                    fontSize={14}
-                                />
-                                <CustomButton 
-                                    title={'Edit Information'}
-                                    icon={<CustomEditIcon color={Colors.white} />}
-                                    onPress={()=> {}}
-                                    color={Colors.blue}
-                                    width={270}
-                                    height={47}
-                                    fontSize={14}
-                                />
-                            </View>
-                            
-                            </>)}
-                    </ScrollView>
-                </View>
-            </BlurView>
-        </Modal>
+                    <ChemicalDetail property={'Purchase Date: '} value={selectedChemical.purchase_date} />
+                    <ChemicalDetail property={'Expiration Date: '} value={selectedChemical.expiration_date} margin={gapSize} />
+
+                    <ChemicalDetail property={'School: '} value={selectedChemical.school} />
+                    <ChemicalDetail property={'Room: '} value={selectedChemical.room} />
+                    <ChemicalDetail property={'Cabinet: '} value={selectedChemical.cabinet} />
+                    <ChemicalDetail property={'Shelf: '} value={selectedChemical.shelf} margin={gapSize} />
+
+                    {/* Status & Quantity */}
+                    <ChemicalDetail property={'Status: '} value={'On-site'} color={getStatusColor('On-site')} />
+                    <ChemicalDetail property={'Quantity: '} value={'Good'} color={getQuantityColor('Good')} margin={gapSize} />
+
+                    {/* Buttons */}
+                    <View style={stylesPopup.buttonContainer}>
+                        <CustomButton
+                            title={'Save QR Label'}
+                            icon={<QrCodeIcon color={Colors.black} strokeWidth='4' />}
+                            textColor={Colors.black}
+                            onPress={() => { }}
+                            color={Colors.white}
+                            width={270}
+                            height={47}
+                            fontSize={14}
+                        />
+                        <CustomButton
+                            title={'View SDS'}
+                            icon={<ViewDocIcon color={Colors.black} />}
+                            textColor={Colors.black}
+                            onPress={toggleSDSBottomSheet}
+                            color={Colors.white}
+                            width={270}
+                            height={47}
+                            fontSize={14}
+                        />
+                        <CustomButton
+                            title={'Edit Information'}
+                            icon={<CustomEditIcon color={Colors.white} />}
+                            onPress={() => { }}
+                            color={Colors.blue}
+                            width={270}
+                            height={47}
+                            fontSize={14}
+                        />
+                    </View>
+
+                </>)}
+            </ScrollContainer>
+        </ModalContainer>
     )
 }
 
 const stylesPopup = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#F5F5F5',
-    },
-    blurContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalView: {
-        width: '100%',
-        height: Size.height(691),
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        backgroundColor: Colors.offwhite,
-        padding: 20,
-        alignItems: 'center',
-        elevation: 20,
-        marginTop: 'auto',
-    },
-    closeButton: {
-        alignSelf: 'flex-end',
-        padding: 0,
-        margin: 0,
-    },
-    closeButtonTextInter: {
-        fontSize: 25,
-        color: Colors.black,
-    },
     modalContent: {
         width: '100%',
         marginTop: 20,
+    },
+    qrTitleContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        marginBottom: Size.height(5)
+    },
+    nameCASContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 5
     },
     buttonContainer: {
         alignItems: 'center',
@@ -206,38 +212,6 @@ const stylesPopup = StyleSheet.create({
         justifyContent: 'center',
         marginBottom: 10,
     },
-    onSiteStatus: {
-        color: '#4285F4',
-        fontWeight: 'bold',
-    },
-    actionButton: {
-        width: '100%',
-        backgroundColor: '#F5F5F5',
-        paddingVertical: 10,
-        borderRadius: 8,
-        alignItems: 'center',
-        marginVertical: 5,
-    },
-    actionButtonTextInter: {
-        fontSize: 16,
-    },
-    editButton: {
-        width: '100%',
-        backgroundColor: '#4285F4',
-        paddingVertical: 10,
-        borderRadius: 8,
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    editButtonTextInter: {
-        color: 'white',
-        fontSize: 16,
-    },
-    details: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    }
 
 });
 
