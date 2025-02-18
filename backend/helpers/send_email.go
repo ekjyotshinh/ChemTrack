5765cba90dc2afefde2e5fdc615eaef6733dd9ab
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-02-18 13:45:26 -0800

    created an email sending helper

---
+++ backend/helpers/send_email.go
@@ -0,0 +1,26 @@
+package helpers
+
+import (
+	"os"
+	"fmt"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+// sendEmailHelper is a helper function to send an email 
+func sendEmailHelper(to, subject, body string) error {
+	from := mail.NewEmail("ChemTrack", "ekjyotshinh@gmail.com") // TODO: Change this to Chemtrack email before deployment
+	toEmail := mail.NewEmail("Recipient", to)
+	message := mail.NewSingleEmail(from, subject, toEmail, body, body)
+
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	response, err := client.Send(message)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode >= 400 {
+		return fmt.Errorf("SendGrid API error, status_code: %d", response.StatusCode)
+	}
+	return nil
+}
