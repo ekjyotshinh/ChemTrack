35622b2b3e279daefc4fbd8befa47721b8b2223b
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-04-21 10:42:14 -0700

    updated to download the QRlabel based on the chemical ID

--- frontend/components/viewChemicalModals/ChemicalDetails.tsx
+++ frontend/components/viewChemicalModals/ChemicalDetails.tsx
@@ -178,7 +178,7 @@ const ChemicalDetails = ({ selectedChemical, toggleSDSBottomSheet, modalVisible,
                             title={'Download QR Label'}
                             icon={<QrCodeIcon color={Colors.black} strokeWidth='4' />}
                             textColor={Colors.black}
-                            onPress={() => downloadFile(`https://storage.googleapis.com/chemtrack-testing2/QRcodes/HisW8WYTGR9UB5TEGMbC.png`)}
+                            onPress={() => downloadFile(`https://storage.googleapis.com/chemtrack-testing2/label/${selectedChemical.id}.pdf`)}
                             color={Colors.white}
                             width={270}
                             height={47}
